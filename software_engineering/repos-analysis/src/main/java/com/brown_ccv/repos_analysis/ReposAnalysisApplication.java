695fd8666d644df843f80d09d1103c9df3b16851
mikuvey <30137716+mikuvey@users.noreply.github.com>
2024-11-12 18:39:28 -0800

    feat: Initial commit

---
+++ software_engineering/repos-analysis/src/main/java/com/brown_ccv/repos_analysis/ReposAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.brown_ccv.repos_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReposAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReposAnalysisApplication.class, args);
+	}
+
+}
