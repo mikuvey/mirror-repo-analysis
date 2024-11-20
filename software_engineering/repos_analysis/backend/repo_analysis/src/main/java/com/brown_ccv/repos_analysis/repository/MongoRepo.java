c1f00d3e2bc6b004e9ce587f0314d2f799c54ca5
mikuvey <30137716+mikuvey@users.noreply.github.com>
2024-11-20 07:54:54 -0800

    fix: change in name

--- software_engineering/repos_analysis/backend/repo_analysis/src/main/java/com/brown_ccv/repos_analysis/repository/MongoRepo.java
+++
@@ -1,9 +0,0 @@
-package com.brown_ccv.repos_analysis.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.brown_ccv.repos_analysis.model.RepositoryInfo;
-
-public interface MongoRepo extends MongoRepository<RepositoryInfo, Long>{
-    
-}
